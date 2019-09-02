1563e356aa7968a476e5ab8d3055ba7ba3bc5195
eboy02wangmx <zzm798681538@126.com>
2019-09-02 13:14:27 +0900

    xu

--- myapp/src/view-model/Album.js
+++ myapp/src/view-model/Album.js
@@ -1,5 +1,5 @@
 /**
- * TOP画面
+ * ALBUM画面
  *
  * see: 画面定義書_G-NHA-002_ALBUM画面_v1.1.xlsx
  */
@@ -8,37 +8,15 @@ export const albumViewModel = {
   name: 'Album',
   data () {
     return {
-      entryNo: null,
-      setEntryNo: null,
-      mail: null,
-      agentCode: null,
-      showPrintListFlag: false,
-      selectIndex: null,
-      pageNo: 1,
-      pageSize: 10,
-      agentList: [],
-      downloading: false,
-      policyNo: null
+      albums :[],
+      filename:'',
+      userid:''
     }
   },
   created () {
-    if (this.$route.query.entryType !== undefined && this.$route.query.entryType !== '') {
-      this.$store.commit(MUTATIONS.HOMEASSIST_SET_ENTRY_TYPE, this.$route.query.entryType)
-    }
-    // this.$store.dispatch(ACTIONS.READ_NOTICE_LIST, {goodsType: '1', entryType: this.$store.state.homeassist.entryType})
-    this.agentList = this.$store.state.configs.agentList
-    this.agentCode = this.$store.state.configs.agentCode
-    this.$watch('$store.state.configs.agentCode', (newValue, oldValue) => {
-      this.agentCode = newValue
-    })
-    this.$watch('$store.state.configs.agentList', (newValue, oldValue) => {
-      this.agentList = newValue
-    })
-    this.$watch('$store.state.homeassist.entryNo', (newValue, oldValue) => {
-      console.log('$store.state.homeassist.entryNo new value is=' + newValue)
-      this.entryNo = newValue
-    })
-    this.policyNo = ''
+    let params = {userid:this.$store.state.myapp.userName};
+    this.$store.dispatch(ACTIONS.MYAPP_USER_ALBUM, params);
+    this.$store.state.myapp.albums
   },
   computed: {
     isError: function () {
@@ -50,6 +28,14 @@ export const albumViewModel = {
       }
       return false
     }
+  },  
+  methods: {
+    remove(album){
+      let params ={filename:album.filename,userid:album.userid}
+      this.$store.dispatch(ACTIONS.MYAPP_ALBUM_REMOVE, params);
+      
+    }
+
   }
 }
 export default {
